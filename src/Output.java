edccc2c15ec9f609cd83344884787d5242597edf
Gisela Rocha <gisela.ra@culiacan.tecnm.mx>
2023-03-27 11:05:50 -0700

    Ya guarda glosario en PDF

--- src/Output.java
+++ src/Output.java
@@ -1,14 +1,56 @@
 package src;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 public class Output {
 
     public Output() { 
     }
 
     public void printLines(Lines lines){
-        for (String line : lines.getLines()) {
-            System.out.println(line);
+        
+
+        try {
+             // Crear un nuevo documento PDF
+             Document document = new Document(PageSize.A4, 50, 50, 50, 50);
+
+             // Crear un objeto PdfWriter para escribir en el archivo PDF
+             PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("Glosario.pdf"));
+ 
+             // Abrir el documento
+             document.open();
+
+             printIntro(document);
+
+             for (String line : lines.getLines()) {
+                String parts[] = line.split("\\:");
+                System.out.println(parts[0].toUpperCase()+":");
+                document.add(new Paragraph(parts[0].toUpperCase()+":"));
+                System.out.println("    " +parts[1] + "\n");
+                document.add(new Paragraph("    " +parts[1] + "\n"));
+            }
+
+            // Cerrar el documento
+            document.close();
+
+            System.out.println("Archivo PDF creado correctamente.");
+ 
+        } catch (DocumentException | FileNotFoundException e) {
+            // TODO: handle exception
         }
+        
+    }
+
+    public void printIntro(Document document) throws DocumentException{
+        System.out.println("GLOSARIO \n");
+        document.add(new Paragraph("GLOSARIO \n"));
     }
     
 }
